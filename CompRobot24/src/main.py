17409f7d3536872a54ddd577b581680ef2637d3b
Ameer <71401181+chickenmcchicken@users.noreply.github.com>
2024-02-06 15:20:31 -0500

    Add files via upload

---
+++ CompRobot24/src/main.py
@@ -0,0 +1,36 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       ameer                                                        #
+# 	Created:      1/30/2024, 4:15:02 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+def main():
+    
+    pass
+
+
+def preauto():
+    pass
+
+def automonus():
+
+
+
+    pass
+
+
+def driver():
+    pass
+        
