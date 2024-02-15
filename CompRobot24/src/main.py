14b7b2e081f46034003ead3477e64b8139cee355
KiwiZ6969420 <154278547+KiwiZ6969420@users.noreply.github.com>
2024-02-15 16:13:03 -0500

    Update main.py

--- CompRobot24/src/main.py
+++ CompRobot24/src/main.py
@@ -14,7 +14,8 @@ from vex import *
 brain=Brain()
 
 brain.screen.print("Hello V5")
-
+controller = Controller(PRIMARY)
+con2 = Controller(PARTNER)
 Left_Motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True) 
 Right_Motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True) 
 Intake = Motor(Ports.PORT6, GearSetting.RATIO_6_1, True)
@@ -34,49 +35,60 @@ def automonus():
 
     pass
 
-con = Controller()
 
-def yuzhen_driver():
-    if controller.axis3.pressing() == True:
-        Left_Motor.spin(FORWARD) 
-        Left_Motor.set.velocity(75, PERCENT)
-    elif controller.axis3.pressing() == True: 
-        Left_Motor.spin(REVERSE)
-        Left_Motor.set.velocity(75, PERCENT)
-    else:
-        Left_Motor.stop()
-    
-    if controller.axis2.pressing() == True: 
+
+
+def primary_joysticks():
+    axis3 = controller.axis3.position()
+    axis4 = controller.axis4.position()
+    total = axis3 + axis4
+    more_total = axis3 - axis4
+    if abs(total) > 5: 
+        Left_Motor.set.velocity(total, PERCENT)
+        Left_Motor.spin(FORWARD)
+        Right_Motor.set.velocity(more_total, PERCENT)
         Right_Motor.spin(FORWARD)
-        Right_Motor.set.velocity(75, PERCENT)
-    elif controller.axis2.pressing() == True: 
-        Right_Motor.spin(REVERSE)
-        Right_Motor.set.velocity(75, PERCENT)
-    else: 
-        Right_Motor.stop()
+        
+
+def primary_driver():
+    while True:
+        primary_joysticks()
+        if controller.axis3.position() == -100: 
+            Left_Motor.spin(REVERSE)
+            Left_Motor.set.velocity(75, PERCENT)
+        else:
+            Left_Motor.stop()
+        
+        if controller.axis2.position() == True: 
+            Right_Motor.spin(FORWARD)
+            Right_Motor.set.velocity(75, PERCENT)
+        elif controller.axis2.position() == True: 
+            Right_Motor.spin(REVERSE)
+            Right_Motor.set.velocity(75, PERCENT)
+        else: 
+            Right_Motor.stop()
+        pass
+
+def Partner_joystick():
+    axis2 = con2.axis2.position()
+    axis1 = con2.axis1.position()
+    leftsped = axis2 - axis1
+    rightsped = axis2 + axis1
+
+    Left_Motor.set_velocity(leftsped, PERCENT)
+    Right_Motor.set_velocity(rightsped, PERCENT)
+    Left_Motor.spin(FORWARD); Right_Motor.spin(FORWARD)
+
     pass
 
-def jaine_drive():
-    if controller.axis2.pressing() == True:
-        Left_Motor.spin(FORWARD)
-        Left_Motor.set.velocity(75, PERCENT)
-        Right_Motor.spin(FORWARD)
-        Right_Motor.set.velocity(75, PERCENT)
-    elif controller.axis2.pressing() == True:
-        Left_Motor.spin(REVERSE)
-        Left_Motor.set.velocity(75, PERCENT)
-        Right_Motor.spin(REVERSE)
-        Right_Motor.set.velocity(75, PERCENT)
-    else:
-        Right_Motor.stop()
-        Left_Motor.stop()
 
-    if controller.axis1.pressing() == True:
-        Left_Motor.spin(FORWARD)
-    elif controller.axis1.pressing() == True:
-        Right_Motor.spin(FORWARD)
-    else
+def Partner_control():
+    
 
     pass
         
+
+
+        
+
         
